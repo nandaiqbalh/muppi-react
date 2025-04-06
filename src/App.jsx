b8ee51242cf331b464f6555f2fa4e479d2c60818
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2025-04-06 07:59:46 +0700

    - Add asset - Header UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
+import Search from "./components/Search.jsx";
+import {useState} from "react";
 
 function App() {
 
+    const [searchTerm, setSearchTerm] = useState('');
+
+
   return (
-      <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      </>
+      <main className="pattern">
+         <div className="wrapper">
+             <header>
+                 <img src="/hero.png" alt=""/>
+                 <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without Hassle</h1>
+             </header>
+
+             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+         </div>
+      </main>
   )
 }
 
